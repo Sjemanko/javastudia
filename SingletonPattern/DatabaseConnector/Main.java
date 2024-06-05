64f5654f210f473ccc8c2b73c3f275e01f7ed230
Tomasz Bułecki <96573032+Sjemanko@users.noreply.github.com>
2024-06-05 23:15:53 +0200

    Add files via upload

--- SingletonPattern/DatabaseConnector/Main.java
+++ SingletonPattern/DatabaseConnector/Main.java
@@ -1,17 +1,18 @@
 package SingletonPattern.DatabaseConnector;
 
+
 public class Main {
 
     public static void main(String[] args)
     {
-        DatabaseConnectorExample DBCexample = DatabaseConnectorExample.getInstance();
+        FakeDatabaseConnector DBCexample = FakeDatabaseConnector.getInstance();
         DBCexample.connect();
         DBCexample.executeQuery("SQL QUERY(1)");
-        System.out.println(DatabaseConnectorExample.counter);
+        System.out.println(FakeDatabaseConnector.counter);
 
-        DatabaseConnectorExample DBCexample2 = DatabaseConnectorExample.getInstance();
+        FakeDatabaseConnector DBCexample2 = FakeDatabaseConnector.getInstance();
         DBCexample2.connect();
         DBCexample2.executeQuery("SQL QUERY(2)");
-        System.out.println(DatabaseConnectorExample.counter);
+        System.out.println(FakeDatabaseConnector.counter);
     }
 }
